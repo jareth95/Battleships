30b8e2ba8ddbf823a44946ddd7cdf12bb26bd4d3
jareth95 <jareth_smith@hotmail.com>
2021-01-03 23:09:34 +0000

    ships rotated

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const computerGrid = document.querySelector('.computer-gameBoard')
     const displayGrid = document.querySelector('.display-ships')
     const ships = document.querySelectorAll('.ship')
-    const destroyer = document.querySelector('.destroyer')
-    const submarine = document.querySelector('.submarine')
-    const cruiser = document.querySelector('.cruiser')
-    const battleship = document.querySelector('.battleship')
-    const carrier = document.querySelector('.carrier')
+    const destroyer = document.querySelector('.destroyer-container')
+    const submarine = document.querySelector('.submarine-container')
+    const cruiser = document.querySelector('.cruiser-container')
+    const battleship = document.querySelector('.battleship-container')
+    const carrier = document.querySelector('.carrier-container')
     const startButton = document.querySelector('#start')
     const rotateButton = document.querySelector('#rotate')
     const turnDisplay = document.querySelector('#whose-go')
     const infoDisplay = document.querySelector('#info')
     const userSquares = []
     const computerSquares = []
+    let isHorizontal = true
+
     const width = 10
 
     // create board
@@ -28,4 +30,82 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     createBoard(userGrid, userSquares)
     createBoard(computerGrid, computerSquares)
+    // ships
+    const shipArray = [
+        { 
+            name: 'destroyer',
+            directions: [
+                [0, 1],
+                [0, width]
+            ]
+        },
+        {
+            name: 'cruiser',
+            directions: [
+                [0, 1, 2],
+                [0, width, width*2]
+            ]
+        },
+        {
+            name: 'submarine',
+            directions: [
+                [0, 1, 2],
+                [0, width, width*2]
+            ]
+        },
+        {
+            name: 'battleship',
+            directions: [
+                [0, 1, 2, 3],
+                [0, width, width*2, width*3]
+            ]
+        },
+        {
+            name: 'carrier',
+            directions: [
+                [0, 1, 2, 3, 4],
+                [0, width, width*2, width*3, width*4]
+            ]
+        },
+
+    ]
+    // Draw the computers ships
+    function generate(ship) {
+        let direction
+        let randomDirection = Math.floor(Math.random() * ship.directions.length)
+        let current = ship.directions[randomDirection]
+        if (randomDirection === 0) direction = 1
+        if (randomDirection === 1) direction = 10
+        let randomStart = Math.abs(Math.floor(Math.random() * computerSquares.length - (ship.directions[0].length * direction)))
+
+        const isTaken = current.some(index => computerSquares[randomStart + index].classList.contains('taken'))
+        const isAtRightEdge = current.some(index => (randomStart + index) % width === width -1)
+        const isAtLeftEdge = current.some(index => (randomStart + index) % width === 0)
+
+        if(!isTaken && !isAtLeftEdge && !isAtRightEdge) current.forEach(index => computerSquares[randomStart + index].classList.add('taken', ship.name))
+
+        else generate(ship)
+    }
+    for (let i = 0; i < shipArray.length; i++){
+        generate(shipArray[i])
+    }
+    //  Rotate ships
+    function rotate() {
+        if (isHorizontal) {
+            destroyer.classList.toggle('destroyer-container-vertical')
+            submarine.classList.toggle('submarine-container-vertical')
+            cruiser.classList.toggle('cruiser-container-vertical')
+            battleship.classList.toggle('battleship-container-vertical')
+            carrier.classList.toggle('carrier-container-vertical')
+            isHorizontal = false
+        } else {
+            destroyer.classList.toggle('destroyer-container-vertical')
+            submarine.classList.toggle('submarine-container-vertical')
+            cruiser.classList.toggle('cruiser-container-vertical')
+            battleship.classList.toggle('battleship-container-vertical')
+            carrier.classList.toggle('carrier-container-vertical')
+            isHorizontal = true
+        }
+    }
+    rotateButton.addEventListener('click', rotate)
 })
